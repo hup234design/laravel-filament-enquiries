afbccb6b175edbc3b7bee0c1c10e6d41266ece59
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2022-08-22 12:07:15 +0100

    style form

--- resources/views/enquiry-form.blade.php
+++ resources/views/enquiry-form.blade.php
@@ -1,31 +1,33 @@
 <div>
-    <form wire:submit.prevent="submit">
 
-        <div class="divide-y p-4">
+        <div class="bg-white p-4">
 
-            <div class="py-4">
-                <input type="text" wire:model="enquiry.name" class="border w-full p-2">
-                @error('enquiry.name') <span class="block text-red-600">{{ $message }}</span> @enderror
-            </div>
-
-            <div class="py-4">
-                <input type="text" wire:model="enquiry.email" class="border w-full p-2">
-                @error('enquiry.email') <span class="block text-red-600">{{ $message }}</span> @enderror
-            </div>
-
-            <div class="py-4">
-                <input type="text" wire:model="enquiry.telephone" class="border w-full p-2">
-                @error('enquiry.telephone') <span class="block text-red-600">{{ $message }}</span> @enderror
-            </div>
-
-            <div class="py-4">
-                <input type="text" wire:model="enquiry.message" class="border w-full p-2">
-                @error('enquiry.message') <span class="block text-red-600">{{ $message }}</span> @enderror
-            </div>
-
-            <button type="submit">Send Message</button>
+                <form wire:submit.prevent="submit" class="grid grid-cols-1 gap-y-6">
+                    <div>
+                        <label for="full-name" class="sr-only">Full name</label>
+                        <input wire:model.lazy="enquiry.name" type="text" name="name" id="full-name" autocomplete="name" class="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" placeholder="Full name">
+                        @error('enquiry.name') <span class="block text-red-600">{{ $message }}</span> @enderror
+                    </div>
+                    <div>
+                        <label for="email" class="sr-only">Email</label>
+                        <input wire:model.lazy="enquiry.email" id="email" name="email" type="email" autocomplete="email" class="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" placeholder="Email">
+                        @error('enquiry.email') <span class="block text-red-600">{{ $message }}</span> @enderror
+                    </div>
+                    <div>
+                        <label for="phone" class="sr-only">Phone</label>
+                        <input wire:model.lazy="enquiry.telephone" type="text" name="telephone" id="telephone" autocomplete="tel" class="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" placeholder="Telephone">
+                        @error('enquiry.telephone') <span class="block text-red-600">{{ $message }}</span> @enderror
+                    </div>
+                    <div>
+                        <label for="message" class="sr-only">Message</label>
+                        <textarea wire:model.lazy="enquiry.message" id="message" name="message" rows="4" class="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border border-gray-300 rounded-md" placeholder="Message"></textarea>
+                        @error('enquiry.message') <span class="block text-red-600">{{ $message }}</span> @enderror
+                    </div>
+                    <div>
+                        <button type="submit" class="inline-flex justify-center py-3 px-6 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Submit</button>
+                    </div>
+                </form>
 
         </div>
 
-    </form>
 </div>
