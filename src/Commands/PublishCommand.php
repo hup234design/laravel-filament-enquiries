441eeb35b4f84f98f235b60dd4a8a86ea4304460
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2022-08-22 11:15:00 +0100

    update command and migration

--- src/Commands/PublishCommand.php
+++ src/Commands/PublishCommand.php
@@ -6,16 +6,16 @@ use Illuminate\Console\Command;
 
 class PublishCommand extends Command
 {
-    public $signature = 'laravel-filament-enquiries:publish {--force : Overwrite any existing files}';
+    public $signature = 'filament-enquiries:publish {--force : Overwrite any existing files}';
 
-    public $description = 'PublishCommand all Laravel Filament Enquiries components and resources';
+    public $description = 'Publish all Laravel Filament Enquiries components and resources';
 
     public function handle(): int
     {
         // publish Filament Enquiries files
-        $this->callSilent('vendor:publish', ['--tag' => 'laravel-filament-enquiries-config', '--force' => $this->option('force') ?? false]);
-        $this->callSilent('vendor:publish', ['--tag' => 'laravel-filament-enquiries-migrations', '--force' => $this->option('force') ?? false]);
-        $this->callSilent('vendor:publish', ['--tag' => 'laravel-filament-enquiries-views', '--force' => $this->option('force') ?? false]);
+        $this->call('vendor:publish', ['--tag' => 'filament-enquiries-config', '--force' => $this->option('force') ?? false]);
+        $this->callSilent('vendor:publish', ['--tag' => 'filament-enquiries-migrations', '--force' => $this->option('force') ?? false]);
+        $this->callSilent('vendor:publish', ['--tag' => 'filament-enquiries-views', '--force' => $this->option('force') ?? false]);
         $this->output->success('HUP234 Filament Enquiries has been Published successfully');
 
         return self::SUCCESS;
--- src/LaravelFilamentEnquiriesServiceProvider.php
+++ src/LaravelFilamentEnquiriesServiceProvider.php
@@ -15,6 +15,7 @@ class LaravelFilamentEnquiriesServiceProvider extends PluginServiceProvider
          *
          * More info: https://github.com/spatie/laravel-package-tools
          */
+        parent::configurePackage($package);
         $package
             ->name('laravel-filament-enquiries')
             ->hasConfigFile()
