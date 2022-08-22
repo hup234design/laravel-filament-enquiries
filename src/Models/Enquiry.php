8b22e81e846175dffb3ff707e263b8fa6a589487
hup234design <hup234design@users.noreply.github.com>
2022-08-22 10:59:06 +0000

    Fix styling

--- src/Models/Enquiry.php
+++ src/Models/Enquiry.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
  * Class Enquiry
  *
  * @link   https://hup234design.co.uk/
+ *
  * @author HUp234 Design <support@hup234design.co.uk>
  * @author Dave Walker <dave@hup234design.co.uk>
  */
